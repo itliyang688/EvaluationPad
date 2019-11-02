edd6a45ca39d4dd5eefb1cee06777c39f448a7ad
liyang <871133438@qq.com>
2019-11-02 17:52:19 +0800

    //报告页面，右侧menu页面

--- app/src/main/java/cn/fek12/evaluation/view/PopupWindow/MenuPopupWindow.java
+++ app/src/main/java/cn/fek12/evaluation/view/PopupWindow/MenuPopupWindow.java
@@ -4,25 +4,18 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.PopupWindow;
 
 import cn.fek12.evaluation.R;
+import cn.fek12.evaluation.view.widget.MultipleStatusView;
 
-/**
- * @Name: MenuPopupWindow
- * @Description: 退出系统
- * @author: LiYang （作者）
- * @version: V1.00 (版本号)
- * @Create Date: 2016-02-16 (创建日期)
- */
+
 public class MenuPopupWindow extends PopupWindow {
 	private View mMenuView;
+	private MultipleStatusView multipleStatusView;
 
 	@SuppressLint("InflateParams")
 	public MenuPopupWindow(Context context, OnClickListener itemsOnClick) {
@@ -30,6 +23,7 @@ public class MenuPopupWindow extends PopupWindow {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		mMenuView = inflater.inflate(R.layout.menu_dialog_activity, null);
+		multipleStatusView = mMenuView.findViewById(R.id.load_view);
 
 		// 设置SelectPicPopupWindow的View
 		this.setContentView(mMenuView);
@@ -46,17 +40,18 @@ public class MenuPopupWindow extends PopupWindow {
 		// 设置SelectPicPopupWindow弹出窗体的背景
 		this.setBackgroundDrawable(dw);
 		// mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
-		mMenuView.setOnTouchListener(new OnTouchListener() {
-			@Override
-			@SuppressLint("ClickableViewAccessibility")
-			public boolean onTouch(View v, MotionEvent event) {
-				if (event.getAction() == MotionEvent.ACTION_UP) {
-					dismiss();
-				}
-				return true;
-			}
-		});
+		mMenuView.setOnClickListener(clickListener);
+		multipleStatusView.setOnClickListener(clickListener);
 
 	}
+
+	private OnClickListener clickListener = new OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			if(view.getId() != R.id.load_view){
+				dismiss();
+			}
+		}
+	};
 
 }
