bf0424d000ef0986614f0e296a5533f1b3d96704
LiYang <liyang@noc.net.cn>
2020-09-29 10:08:24 +0800

    学情模块开发

--- app/src/main/java/cn/fek12/evaluation/application/MyApplication.java
+++ app/src/main/java/cn/fek12/evaluation/application/MyApplication.java
@@ -36,7 +36,6 @@ public class MyApplication extends BaseApplication {
     private static final int DEFAULT_TIMEOUT = 30;
     //private String userId = "413";
     //private String userId = "8a236697-cd63-45e5-b502-823de2dec1ba";//yanglaoshi
-    //private String userId = "094ed121-cc66-46d1-892a-0be39da5b206";
     private String userId = "8a236697-cd63-45e5-b502-823de2dec1ba";
     //private String userId = "ece5924a-a24d-4f01-a9e6-5ff25fc8be56";
     private static final String TAG = "AIDL_Log";
